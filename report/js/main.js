a67c91c95e45518e255a72ae5765feedc7aedaa2
Binoculars7 <elitespot.com@gmail.com>
2025-11-15 16:53:56 +0100

    v1

--- report/js/main.js
+++ report/js/main.js
@@ -61,5 +61,276 @@
         ]
     });
     
+    // State to LGA mapping
+    var stateLGAs = {
+        'abia': ['Aba North', 'Aba South', 'Arochukwu', 'Bende', 'Ikwuano', 'Isiala Ngwa North', 'Isiala Ngwa South', 'Isuikwuato', 'Obi Ngwa', 'Ohafia', 'Osisioma', 'Ugwunagbo', 'Ukwa East', 'Ukwa West', 'Umuahia North', 'Umuahia South', 'Umu Nneochi'],
+        'adamawa': ['Demsa', 'Fufure', 'Ganye', 'Gayuk', 'Gombi', 'Grie', 'Hong', 'Jada', 'Larmurde', 'Madagali', 'Maiha', 'Mayo Belwa', 'Michika', 'Mubi North', 'Mubi South', 'Numan', 'Shelleng', 'Song', 'Toungo', 'Yola North', 'Yola South'],
+        'akwa_ibom': ['Abak', 'Eastern Obolo', 'Eket', 'Esit Eket', 'Essien Udim', 'Etim Ekpo', 'Etinan', 'Ibeno', 'Ibesikpo Asutan', 'Ibiono-Ibom', 'Ikot Abasi', 'Ikot Ekpene', 'Ini', 'Itu', 'Mbo', 'Mkpat-Enin', 'Nsit-Atai', 'Nsit-Ibom', 'Nsit-Ubium', 'Obot Akara', 'Okobo', 'Onna', 'Oron', 'Oruk Anam', 'Udung-Uko', 'Ukanafun', 'Uruan', 'Urue-Offong/Oruko', 'Uyo'],
+        'anambra': ['Aguata', 'Anambra East', 'Anambra West', 'Anaocha', 'Awka North', 'Awka South', 'Ayamelum', 'Dunukofia', 'Ekwusigo', 'Idemili North', 'Idemili South', 'Ihiala', 'Njikoka', 'Nnewi North', 'Nnewi South', 'Ogbaru', 'Onitsha North', 'Onitsha South', 'Orumba North', 'Orumba South', 'Oyi'],
+        'bauchi': ['Alkaleri', 'Bauchi', 'Bogoro', 'Damban', 'Darazo', 'Dass', 'Gamawa', 'Ganjuwa', 'Giade', 'Itas/Gadau', 'Jama\'are', 'Katagum', 'Kirfi', 'Misau', 'Ningi', 'Shira', 'Tafawa Balewa', 'Toro', 'Warji', 'Zaki'],
+        'bayelsa': ['Brass', 'Ekeremor', 'Kolokuma/Opokuma', 'Nembe', 'Ogbia', 'Sagbama', 'Southern Ijaw', 'Yenagoa'],
+        'benue': ['Ado', 'Agatu', 'Apa', 'Buruku', 'Gboko', 'Guma', 'Gwer East', 'Gwer West', 'Katsina-Ala', 'Konshisha', 'Kwande', 'Logo', 'Makurdi', 'Obi', 'Ogbadibo', 'Ohimini', 'Oju', 'Okpokwu', 'Otukpo', 'Tarka', 'Ukum', 'Ushongo', 'Vandeikya'],
+        'borno': ['Abadam', 'Askira/Uba', 'Bama', 'Bayo', 'Biu', 'Chibok', 'Damboa', 'Dikwa', 'Gubio', 'Guzamala', 'Gwoza', 'Hawul', 'Jere', 'Kaga', 'Kala/Balge', 'Konduga', 'Kukawa', 'Kwaya Kusar', 'Mafa', 'Magumeri', 'Maiduguri', 'Marte', 'Mobbar', 'Monguno', 'Ngala', 'Nganzai', 'Shani'],
+        'cross_river': ['Abi', 'Akpabuyo', 'Akwa', 'Bakassi', 'Bekwarra', 'Biase', 'Boki', 'Calabar Municipal', 'Calabar South', 'Etung', 'Ikom', 'Obanliku', 'Obubra', 'Obudu', 'Odukpani', 'Ogoja', 'Yakuur', 'Yala'],
+        'delta': ['Aniocha North', 'Aniocha South', 'Bomadi', 'Burutu', 'Ethiope East', 'Ethiope West', 'Ika North East', 'Ika South', 'Isoko North', 'Isoko South', 'Ndokwa East', 'Ndokwa West', 'Okpe', 'Oshimili North', 'Oshimili South', 'Patani', 'Sapele', 'Udu', 'Ughelli North', 'Ughelli South', 'Ukwuani', 'Uvwie', 'Warri North', 'Warri South', 'Warri South West'],
+        'ebonyi': ['Abakaliki', 'Afikpo North', 'Afikpo South', 'Ebonyi', 'Ezza North', 'Ezza South', 'Ikwo', 'Ishielu', 'Ivo', 'Izzi', 'Ohaozara', 'Ohaukwu', 'Onicha'],
+        'edo': ['Akoko-Edo', 'Egor', 'Esan Central', 'Esan North-East', 'Esan South-East', 'Esan West', 'Etsako Central', 'Etsako East', 'Etsako West', 'Igueben', 'Ikpoba Okha', 'Oredo', 'Orhionmwon', 'Ovia North-East', 'Ovia South-West', 'Owan East', 'Owan West', 'Uhunmwonde'],
+        'ekiti': ['Ado Ekiti', 'Efon', 'Ekiti East', 'Ekiti South-West', 'Ekiti West', 'Emure', 'Gbonyin', 'Ido Osi', 'Ijero', 'Ikere', 'Ikole', 'Ilejemeje', 'Irepodun/Ifelodun', 'Ise/Orun', 'Moba', 'Oye'],
+        'enugu': ['Aninri', 'Awgu', 'Enugu East', 'Enugu North', 'Enugu South', 'Ezeagu', 'Igbo Etiti', 'Igbo Eze North', 'Igbo Eze South', 'Isi Uzo', 'Nkanu East', 'Nkanu West', 'Nsukka', 'Oji River', 'Udenu', 'Udi', 'Uzo Uwani'],
+        'gombe': ['Akko', 'Balanga', 'Billiri', 'Dukku', 'Funakaye', 'Gombe', 'Kaltungo', 'Kwami', 'Nafada', 'Shongom', 'Yamaltu/Deba'],
+        'imo': ['Aboh Mbaise', 'Ahiazu Mbaise', 'Ehime Mbano', 'Ezinihitte', 'Ideato North', 'Ideato South', 'Ihitte/Uboma', 'Ikeduru', 'Isiala Mbano', 'Isu', 'Mbaitoli', 'Ngor Okpala', 'Njaba', 'Nkwerre', 'Nwangele', 'Obowo', 'Oguta', 'Ohaji/Egbema', 'Okigwe', 'Orlu', 'Orsu', 'Oru East', 'Oru West', 'Owerri Municipal', 'Owerri North', 'Owerri West', 'Unuimo'],
+        'jigawa': ['Auyo', 'Babura', 'Biriniwa', 'Birnin Kudu', 'Buji', 'Dutse', 'Gagarawa', 'Garki', 'Gumel', 'Guri', 'Gwaram', 'Gwiwa', 'Hadejia', 'Jahun', 'Kafin Hausa', 'Kazaure', 'Kiri Kasama', 'Kiyawa', 'Kaugama', 'Maigatari', 'Malam Madori', 'Miga', 'Ringim', 'Roni', 'Sule Tankarkar', 'Taura', 'Yankwashi'],
+        'kaduna': ['Birnin Gwari', 'Chikun', 'Giwa', 'Igabi', 'Ikara', 'Jaba', 'Jema\'a', 'Kachia', 'Kaduna North', 'Kaduna South', 'Kagarko', 'Kajuru', 'Kaura', 'Kauru', 'Kubau', 'Kudan', 'Lere', 'Makarfi', 'Sabon Gari', 'Sanga', 'Soba', 'Zangon Kataf', 'Zaria'],
+        'kano': ['Ajingi', 'Albasu', 'Bagwai', 'Bebeji', 'Bichi', 'Bunkure', 'Dala', 'Dambatta', 'Dawakin Kudu', 'Dawakin Tofa', 'Doguwa', 'Fagge', 'Gabasawa', 'Garko', 'Garun Mallam', 'Gaya', 'Gezawa', 'Gwale', 'Gwarzo', 'Kabo', 'Kano Municipal', 'Karaye', 'Kibiya', 'Kiru', 'Kumbotso', 'Kunchi', 'Kura', 'Madobi', 'Makoda', 'Minjibir', 'Nasarawa', 'Rano', 'Rimin Gado', 'Rogo', 'Shanono', 'Sumaila', 'Takai', 'Tarauni', 'Tofa', 'Tsanyawa', 'Tudun Wada', 'Ungogo', 'Warawa', 'Wudil'],
+        'katsina': ['Bakori', 'Batagarawa', 'Batsari', 'Baure', 'Bindawa', 'Charanchi', 'Dandume', 'Danja', 'Dan Musa', 'Daura', 'Dutsi', 'Dutsin Ma', 'Faskari', 'Funtua', 'Ingawa', 'Jibia', 'Kafur', 'Kaita', 'Kankara', 'Kankia', 'Katsina', 'Kurfi', 'Kusada', 'Mai\'Adua', 'Malumfashi', 'Mani', 'Mashi', 'Matazu', 'Musawa', 'Rimi', 'Sabuwa', 'Safana', 'Sandamu', 'Zango'],
+        'kebbi': ['Aleiro', 'Arewa Dandi', 'Argungu', 'Augie', 'Bagudo', 'Bunza', 'Dandi', 'Fakai', 'Gwandu', 'Jega', 'Kalgo', 'Koko/Besse', 'Maiyama', 'Ngaski', 'Sakaba', 'Shanga', 'Suru', 'Wasagu/Danko', 'Yauri', 'Zuru'],
+        'kogi': ['Adavi', 'Ajaokuta', 'Ankpa', 'Bassa', 'Dekina', 'Ibaji', 'Idah', 'Igalamela Odolu', 'Ijumu', 'Kabba/Bunu', 'Kogi', 'Lokoja', 'Mopa Muro', 'Ofu', 'Ogori/Magongo', 'Okehi', 'Okene', 'Olamaboro', 'Omala', 'Yagba East', 'Yagba West'],
+        'kwara': ['Asa', 'Baruten', 'Edu', 'Ekiti', 'Ifelodun', 'Ilorin East', 'Ilorin South', 'Ilorin West', 'Irepodun', 'Isin', 'Kaiama', 'Moro', 'Offa', 'Oke Ero', 'Oyun', 'Pategi'],
+        'lagos': ['Agege', 'Ajeromi-Ifelodun', 'Alimosho', 'Amuwo-Odofin', 'Apapa', 'Badagry', 'Epe', 'Eti Osa', 'Ibeju-Lekki', 'Ifako-Ijaiye', 'Ikeja', 'Ikorodu', 'Kosofe', 'Lagos Island', 'Lagos Mainland', 'Mushin', 'Ojo', 'Oshodi-Isolo', 'Shomolu', 'Surulere'],
+        'nasarawa': ['Akwanga', 'Awe', 'Doma', 'Karu', 'Keana', 'Keffi', 'Kokona', 'Lafia', 'Nasarawa', 'Nasarawa Egon', 'Obi', 'Toto', 'Wamba'],
+        'niger': ['Agaie', 'Agwara', 'Bida', 'Borgu', 'Bosso', 'Chanchaga', 'Edati', 'Gbako', 'Gurara', 'Katcha', 'Kontagora', 'Lapai', 'Lavun', 'Magama', 'Mariga', 'Mashegu', 'Mokwa', 'Moya', 'Paikoro', 'Rafi', 'Rijau', 'Shiroro', 'Suleja', 'Tafa', 'Wushishi'],
+        'ogun': ['Abeokuta North', 'Abeokuta South', 'Ado-Odo/Ota', 'Egbado North', 'Egbado South', 'Ewekoro', 'Ifo', 'Ijebu East', 'Ijebu North', 'Ijebu North East', 'Ijebu Ode', 'Ikenne', 'Imeko Afon', 'Ipokia', 'Obafemi Owode', 'Odeda', 'Odogbolu', 'Ogun Waterside', 'Remo North', 'Shagamu'],
+        'ondo': ['Akoko North-East', 'Akoko North-West', 'Akoko South-West', 'Akoko South-East', 'Akure North', 'Akure South', 'Ese Odo', 'Idanre', 'Ifedore', 'Ilaje', 'Ile Oluji/Okeigbo', 'Irele', 'Odigbo', 'Okitipupa', 'Ondo East', 'Ondo West', 'Ose', 'Owo'],
+        'osun': ['Atakunmosa East', 'Atakunmosa West', 'Aiyedaade', 'Aiyedire', 'Boluwaduro', 'Boripe', 'Ede North', 'Ede South', 'Ife Central', 'Ife East', 'Ife North', 'Ife South', 'Egbedore', 'Ejigbo', 'Ifedayo', 'Ifelodun', 'Ila', 'Ilesa East', 'Ilesa West', 'Irepodun', 'Irewole', 'Isokan', 'Iwo', 'Obokun', 'Odo Otin', 'Ola Oluwa', 'Olorunda', 'Oriade', 'Orolu', 'Osogbo'],
+        'oyo': ['Afijio', 'Akinyele', 'Atiba', 'Atisbo', 'Egbeda', 'Ibadan North', 'Ibadan North-East', 'Ibadan North-West', 'Ibadan South-West', 'Ibadan South-East', 'Ibarapa Central', 'Ibarapa East', 'Ibarapa North', 'Ido', 'Irepo', 'Iseyin', 'Itesiwaju', 'Iwajowa', 'Kajola', 'Lagelu', 'Ogbomoso North', 'Ogbomoso South', 'Ogo Oluwa', 'Olorunsogo', 'Oluyole', 'Ona Ara', 'Orelope', 'Ori Ire', 'Oyo', 'Oyo East', 'Saki East', 'Saki West', 'Surulere'],
+        'plateau': ['Barkin Ladi', 'Bassa', 'Bokkos', 'Jos East', 'Jos North', 'Jos South', 'Kanam', 'Kanke', 'Langtang North', 'Langtang South', 'Mangu', 'Mikang', 'Pankshin', 'Qua\'an Pan', 'Riyom', 'Shendam', 'Wase'],
+        'rivers': ['Abua/Odual', 'Ahoada East', 'Ahoada West', 'Akuku-Toru', 'Andoni', 'Asari-Toru', 'Bonny', 'Degema', 'Eleme', 'Emuoha', 'Etche', 'Gokana', 'Ikwerre', 'Khana', 'Obio/Akpor', 'Ogba/Egbema/Ndoni', 'Ogu/Bolo', 'Okrika', 'Omuma', 'Opobo/Nkoro', 'Oyigbo', 'Port Harcourt', 'Tai'],
+        'sokoto': ['Binji', 'Bodinga', 'Dange Shuni', 'Gada', 'Goronyo', 'Gudu', 'Gwadabawa', 'Illela', 'Isa', 'Kebbe', 'Kware', 'Rabah', 'Sabon Birni', 'Shagari', 'Silame', 'Sokoto North', 'Sokoto South', 'Tambuwal', 'Tangaza', 'Tureta', 'Wamako', 'Wurno', 'Yabo'],
+        'taraba': ['Ardo Kola', 'Bali', 'Donga', 'Gashaka', 'Gassol', 'Ibi', 'Jalingo', 'Karim Lamido', 'Kumi', 'Lau', 'Sardauna', 'Takum', 'Ussa', 'Wukari', 'Yorro', 'Zing'],
+        'yobe': ['Bade', 'Bursari', 'Damaturu', 'Fika', 'Fune', 'Geidam', 'Gujba', 'Gulani', 'Jakusko', 'Karasuwa', 'Machina', 'Nangere', 'Nguru', 'Potiskum', 'Tarmuwa', 'Yunusari', 'Yusufari'],
+        'zamfara': ['Anka', 'Bakura', 'Birnin Magaji/Kiyaw', 'Bukkuyum', 'Bungudu', 'Gummi', 'Kaura Namoda', 'Katsina', 'Maradun', 'Maru', 'Shinkafi', 'Talata Mafara', 'Chafe', 'Zurmi'],
+        'fct': ['Abaji', 'Bwari', 'Gwagwalada', 'Kuje', 'Kwali', 'Municipal Area Council']
+    };
+    
+    // Populate LGA dropdown based on selected state
+    $('#stateSelect').on('change', function() {
+        var selectedState = $(this).val();
+        var lgaSelect = $('#lgaSelect');
+        
+        // Clear existing options except the first one
+        lgaSelect.find('option:not(:first)').remove();
+        
+        // If a state is selected, populate LGAs
+        if (selectedState && stateLGAs[selectedState]) {
+            var lgas = stateLGAs[selectedState];
+            lgas.forEach(function(lga) {
+                lgaSelect.append($('<option></option>')
+                    .attr('value', lga.toLowerCase().replace(/\s+/g, '_'))
+                    .text(lga)
+                    .css('color', 'black'));
+            });
+        }
+    });
+    
+    // State name mapping for geocoding responses
+    var stateNameMapping = {
+        'abia': ['abia'],
+        'adamawa': ['adamawa'],
+        'akwa_ibom': ['akwa ibom', 'akwa-ibom'],
+        'anambra': ['anambra'],
+        'bauchi': ['bauchi'],
+        'bayelsa': ['bayelsa'],
+        'benue': ['benue'],
+        'borno': ['borno'],
+        'cross_river': ['cross river'],
+        'delta': ['delta'],
+        'ebonyi': ['ebonyi'],
+        'edo': ['edo'],
+        'ekiti': ['ekiti'],
+        'enugu': ['enugu'],
+        'gombe': ['gombe'],
+        'imo': ['imo'],
+        'jigawa': ['jigawa'],
+        'kaduna': ['kaduna'],
+        'kano': ['kano'],
+        'katsina': ['katsina'],
+        'kebbi': ['kebbi'],
+        'kogi': ['kogi'],
+        'kwara': ['kwara'],
+        'lagos': ['lagos'],
+        'nasarawa': ['nasarawa'],
+        'niger': ['niger'],
+        'ogun': ['ogun'],
+        'ondo': ['ondo'],
+        'osun': ['osun'],
+        'oyo': ['oyo'],
+        'plateau': ['plateau'],
+        'rivers': ['rivers'],
+        'sokoto': ['sokoto'],
+        'taraba': ['taraba'],
+        'yobe': ['yobe'],
+        'zamfara': ['zamfara'],
+        'fct': ['fct', 'federal capital territory', 'abuja']
+    };
+    
+    // Function to find state value from state name
+    function findStateValue(stateName) {
+        if (!stateName) return null;
+        var lowerStateName = stateName.toLowerCase().trim();
+        for (var key in stateNameMapping) {
+            if (stateNameMapping[key].indexOf(lowerStateName) !== -1) {
+                return key;
+            }
+        }
+        return null;
+    }
+    
+    // Function to find LGA from address components
+    function findLGAFromAddress(addressComponents, stateValue) {
+        if (!stateValue || !stateLGAs[stateValue]) return null;
+        
+        var lgas = stateLGAs[stateValue];
+        var addressLower = addressComponents.toLowerCase();
+        
+        // Try to find LGA in address components
+        for (var i = 0; i < lgas.length; i++) {
+            var lgaLower = lgas[i].toLowerCase();
+            if (addressLower.indexOf(lgaLower) !== -1) {
+                return lgas[i];
+            }
+        }
+        return null;
+    }
+    
+    // Autodetect location functionality
+    $('#autodetectLink').on('click', function(e) {
+        e.preventDefault();
+        
+        var link = $(this);
+        var originalText = link.text();
+        
+        // Update link text to show loading
+        link.text('Detecting location...').css('pointer-events', 'none');
+        
+        // Check if geolocation is supported
+        if (!navigator.geolocation) {
+            alert('Geolocation is not supported by your browser.');
+            link.text(originalText).css('pointer-events', 'auto');
+            return;
+        }
+        
+        // Get current position
+        navigator.geolocation.getCurrentPosition(
+            function(position) {
+                var lat = position.coords.latitude;
+                var lon = position.coords.longitude;
+                
+                // Set GPS coordinates
+                $('#gpsInput').val(lat + ', ' + lon);
+                
+                // Reverse geocode using OpenStreetMap Nominatim
+                // Using CORS proxy to avoid CORS issues
+                var nominatimUrl = 'https://nominatim.openstreetmap.org/reverse?format=json&lat=' + lat + '&lon=' + lon + '&zoom=18&addressdetails=1';
+                var proxyUrl = 'https://api.allorigins.win/raw?url=' + encodeURIComponent(nominatimUrl);
+                
+                // Function to process geocoding response
+                function processGeocodingResponse(data) {
+                    if (data && data.address) {
+                        var address = data.address;
+                        var displayName = data.display_name || '';
+                        
+                        // Set address
+                        $('#addressInput').val(displayName);
+                        
+                        // Try to find state from address components
+                        var stateName = address.state || address.region || address.province || '';
+                        var stateValue = findStateValue(stateName);
+                        
+                        if (stateValue) {
+                            // Set state dropdown
+                            $('#stateSelect').val(stateValue).trigger('change');
+                            
+                            // Try to find LGA
+                            var lgaName = findLGAFromAddress(displayName, stateValue);
+                            if (lgaName) {
+                                // Wait a bit for LGA dropdown to populate
+                                setTimeout(function() {
+                                    var lgaValue = lgaName.toLowerCase().replace(/\s+/g, '_');
+                                    $('#lgaSelect').val(lgaValue);
+                                }, 300);
+                            }
+                        } else {
+                            // If state not found, try searching in display name
+                            var displayLower = displayName.toLowerCase();
+                            for (var key in stateNameMapping) {
+                                var stateNames = stateNameMapping[key];
+                                for (var i = 0; i < stateNames.length; i++) {
+                                    if (displayLower.indexOf(stateNames[i]) !== -1) {
+                                        $('#stateSelect').val(key).trigger('change');
+                                        var lgaName = findLGAFromAddress(displayName, key);
+                                        if (lgaName) {
+                                            setTimeout(function() {
+                                                var lgaValue = lgaName.toLowerCase().replace(/\s+/g, '_');
+                                                $('#lgaSelect').val(lgaValue);
+                                            }, 300);
+                                        }
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                        
+                        link.text(originalText).css('pointer-events', 'auto');
+                    } else {
+                        alert('Could not retrieve address information. GPS coordinates have been set.');
+                        link.text(originalText).css('pointer-events', 'auto');
+                    }
+                }
+                
+                // Try direct request first, then fallback to proxy if needed
+                $.ajax({
+                    url: nominatimUrl,
+                    method: 'GET',
+                    dataType: 'json',
+                    headers: {
+                        'User-Agent': 'SafeNaija Location Service'
+                    },
+                    success: function(data) {
+                        processGeocodingResponse(data);
+                    },
+                    error: function() {
+                        // Fallback to CORS proxy
+                        $.ajax({
+                            url: proxyUrl,
+                            method: 'GET',
+                            dataType: 'json',
+                            success: function(data) {
+                                processGeocodingResponse(data);
+                            },
+                            error: function() {
+                                alert('Error retrieving address information. GPS coordinates have been set.');
+                                link.text(originalText).css('pointer-events', 'auto');
+                            }
+                        });
+                    }
+                });
+            },
+            function(error) {
+                var errorMsg = 'Error getting location: ';
+                switch(error.code) {
+                    case error.PERMISSION_DENIED:
+                        errorMsg += 'User denied the request for Geolocation.';
+                        break;
+                    case error.POSITION_UNAVAILABLE:
+                        errorMsg += 'Location information is unavailable.';
+                        break;
+                    case error.TIMEOUT:
+                        errorMsg += 'The request to get user location timed out.';
+                        break;
+                    default:
+                        errorMsg += 'An unknown error occurred.';
+                        break;
+                }
+                alert(errorMsg);
+                link.text(originalText).css('pointer-events', 'auto');
+            },
+            {
+                enableHighAccuracy: true,
+                timeout: 10000,
+                maximumAge: 0
+            }
+        );
+    });
+    
 })(jQuery);
 
